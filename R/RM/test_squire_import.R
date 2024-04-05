420d9e58aed9dfe2bbe25339f421cd11a65525aa
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2024-04-05 15:20:43 +0100

    check points for whether or not squire loaded into R

--- R/RM/test_squire_import.R
+++ R/RM/test_squire_import.R
@@ -1,14 +1,16 @@
 ## can we run compartmental-mpox like we would squire?
 
+"squire" %in% loadedNamespaces() ### want to be FALSE
+
 library(odin)
 odin::can_compile()
 
-library(devtools)
-
-Sys.getenv("GITHUB_PAT")
-Sys.unsetenv("GITHUB_PAT")
-
-devtools::install_github("mrc-ide/compartmental-mpox") ### because private?
+# library(devtools)
+# 
+# Sys.getenv("GITHUB_PAT")
+# Sys.unsetenv("GITHUB_PAT")
+# 
+# devtools::install_github("mrc-ide/compartmental-mpox") ### didn't work, maybe because private?
 
 ## instead have to run manually to check is working
 
@@ -37,23 +39,35 @@ r2 <- run_explicit_SEEIR_model(country="Afghanistan")
 plot(r2)
 ### THIS RAN YAY
 
+"squire" %in% loadedNamespaces() ## TRUE (something must load the package when running the code?)
+## would have a dependency if so
+## happens during run_explicit_SEEIR_model
+
 
 ## now back to simple_SEIR
 ## last error with contact_matrix_set
 ## from ?squire::run_simple_SEEIR_model
 
 pop <- get_population("Afghanistan", simple_SEIR = TRUE)
+## this loads squire 
 # m1 <- run_simple_SEEIR_model(population = pop$n, dt = 1,
 #                              R0 = 2,
 #                              contact_matrix_set=contact_matrices[[1]])
+unloadNamespace("squire")
 
 contact_matrices <- get_mixing_matrix(country="Afghanistan")
+## this loads squire 
+unloadNamespace("squire")
 
 r <- run_simple_SEEIR_model(population=pop$n,
                             dt=1,
                             R0=2,
                             contact_matrix_set = contact_matrices)
+## but this doesn't load squire
 plot(r)
 
-
 ## THIS ALSO RAN!
+
+
+
+
