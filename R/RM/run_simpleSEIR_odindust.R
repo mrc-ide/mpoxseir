e79e1a51cc4b00cd97b97022f818cd8aa9204673
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2024-04-16 09:57:52 +0100

    script to run odin.dust model

--- R/RM/run_simpleSEIR_odindust.R
+++ R/RM/run_simpleSEIR_odindust.R
@@ -134,3 +134,103 @@ mtext("Number of individuals", side=2,line=1, outer=T)
 mtext("Time", side = 1, line = 0, outer =T)
 
 
+
+
+### now want to run the updated version with (non-age-strat) deaths
+
+
+seird <- odin.dust::odin_dust("inst/odin/simple_SEIR_odindust.R") 
+
+data(polymod, package = "socialmixr")
+age.limits = seq(0, 70, 10)
+contact <- socialmixr::contact_matrix(
+  survey = polymod,
+  countries = "GB",
+  age.limits = age.limits,
+  symmetric = TRUE)
+
+transmission <- contact$matrix /
+  rep(contact$demography$population, each = ncol(contact$matrix))
+transmission
+
+N_age <- length(age.limits)
+n_particles <- 5L
+dt <- 1
+
+
+
+
+# ##Initial vectors
+# S0[] <- user()
+# E0[] <- user()
+# E02[] <- user()
+# Ir0[] <- user()
+# Id0[] <- user()
+# R0[] <- user()
+# D0[] <- user()
+# 
+# ##Parameters
+# beta <- user()
+# gamma_E <- user()
+# gamma_I <- user()
+# gamma_Ir <- user()
+# gamma_Id <- user()
+# CFR[] <- user() #CFR
+# 
+# #Number of age classes & number of transmissibility classes
+# N_age <- user()
+
+
+model <- seird$new(pars = list(dt = 1,
+                               S0 = contact$demography$population,
+                               E0 = c(0, 0, 0, 0, 0, 0, 0, 0),
+                               E02 = c(0, 0, 0, 0, 0, 0, 0, 0),
+                               Ir0 = c(0, 1, 0, 0, 0, 0, 0, 0),
+                               Id0 = c(0, 0, 0, 0, 0, 0, 0, 0),
+                               R0 = c(0, 0, 0, 0, 0, 0, 0, 0),
+                               D0 = c(0, 0, 0, 0, 0, 0, 0, 0),
+                               beta = 0.2 / 12.11,
+                               gamma_E = 0.05,
+                               gamma_I = 0.1,
+                               gamma_Ir = 0.1,
+                               gamma_Id = 0.05,
+                               CFR = c(0.102,0.035,0.02,0.013,0.012,0.012,0.012,0.012),
+                               m = transmission,
+                               N_age = N_age),
+                           time = 1,
+                           n_particles = 5L,
+                           n_threads = 1L,
+                           seed = 1L)
+
+
+# seirds_col <- c("#8c8cd9", "#e67300", "#d279a6", "#ff4d4d", "#999966",
+#                 "#660000")
+# 
+# set.seed(1)
+# x_res <- model$run(365)
+# par(mar = c(4.1, 5.1, 0.5, 0.5), las = 1)
+# matplot(x_res[, 1], x_res[, -1], xlab = "Time", ylab = "Number of individuals",
+#         type = "l", col = seirds_col, lty = 1) ### these are not the correct columns to pull out I think
+# legend("left", lwd = 1, col = seirds_col,
+#        legend = c("S", "E", "Ir", "Id", "R", "D"), bty = "n")
+# 
+# 
+# 
+# x_res
+
+n_times <- 1000
+
+### from odin.dust help
+# Create an array to contain outputs after looping the model.
+# Array contains 35 rows = Total S, I, R (3), and
+# in each age compartment (24) as well as the cumulative incidence (8)
+x <- array(NA, dim = c(model$info()$len, n_particles, n_times))
+
+# For loop to run the model iteratively
+for (t in seq_len(n_times)) {
+  x[ , , t] <- model$run(t)
+}
+
+
+
+
