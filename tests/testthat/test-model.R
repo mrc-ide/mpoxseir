cb37d685578a937ea8f1634002521f5a87bbabaf
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2024-05-30 16:25:45 +0100

    test age-structured zoonotic infections working as expected

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -14,9 +14,10 @@ test_that("run is equal to reference", {
 })
 
 
-test_that("when beta = 0 there are no new infections", {
+test_that("when beta_h = beta_z = 0 there are no new infections", {
   pars <- reference_pars()
-  pars$beta_h <- pars$beta_z <- 0
+  pars$beta_h <- 0
+  pars$beta_z<- rep(0,pars$n_group)
 
   m <- model$new(pars, 1, 3, seed = 1)
   t <- seq(1, 21)
@@ -57,4 +58,19 @@ test_that("when CFR = 1 everybody dies", {
   expect_true(all(res["R_tot", , ] == 0))
   expect_true(any(res["D_tot", , ] > 0))
   expect_equal(sum(res["N_tot", , ] - sum(pars$N)), 0)
+})
+
+
+test_that("when beta_h = 0 there are only zoonotic infections", {
+  pars <- reference_pars()
+  pars$beta_h <- 0
+  pars$beta_z<- c(rep(0,pars$n_group-1),0.4 / 12.11) # last group only for test purpose
+  
+  m <- model$new(pars, 1, 3, seed = 1)
+  t <- seq(1, 21)
+  res <- m$simulate(t)
+  rownames(res) <- names(unlist(m$info()$index))
+  
+  expect_true(all(res[grep("Ea",rownames(res))[1:pars$n_group-1],,]==0))
+  expect_true(all(res[grep("Eb",rownames(res))[1:pars$n_group-1],,]==0))
 })
