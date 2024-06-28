5524850a6d501ceff5618a44a1b82764855c8f39
Lilith Whittles <lwhittles@users.noreply.github.com>
2024-06-28 11:22:45 +0100

    add tests

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -7,8 +7,8 @@ test_that("run is equal to reference", {
   res <- m$simulate(t)
   rownames(res) <- names(unlist(m$info()$index))
 
-  expect_true(any(res["cases", , ] > 0))
-  expect_true(any(res["deaths", , ] > 0))
+  expect_true(any(res["weekly_cases", , ] > 0))
+  expect_true(any(res["weekly_deaths", , ] > 0))
 
   expect_equal(sum(res["N_tot", , ] - sum(pars$N)), 0)
 })
@@ -24,7 +24,7 @@ test_that("when beta_h = beta_z = 0 there are no new infections", {
   res <- m$simulate(t)
   rownames(res) <- names(unlist(m$info()$index))
 
-  expect_true(all(res["cases", , ] == 0))
+  expect_true(all(res["weekly_cases", , ] == 0))
   expect_equal(sum(res["N_tot", , ] - sum(pars$N)), 0)
 })
 
@@ -37,8 +37,8 @@ test_that("when CFR = 0 nobody dies", {
   res <- m$simulate(t)
   rownames(res) <- names(unlist(m$info()$index))
 
-  expect_true(any(res["cases", , ] > 0))
-  expect_true(all(res["deaths", , ] == 0))
+  expect_true(any(res["weekly_cases", , ] > 0))
+  expect_true(all(res["weekly_deaths", , ] == 0))
   expect_true(any(res["R_tot", , ] > 0))
   expect_true(all(res["D_tot", , ] == 0))
   expect_equal(sum(res["N_tot", , ] - sum(pars$N)), 0)
@@ -53,8 +53,8 @@ test_that("when CFR = 1 everybody dies", {
   res <- m$simulate(t)
   rownames(res) <- names(unlist(m$info()$index))
 
-  expect_true(any(res["cases", , ] > 0))
-  expect_true(any(res["deaths", , ] > 0))
+  expect_true(any(res["weekly_cases", , ] > 0))
+  expect_true(any(res["weekly_deaths", , ] > 0))
   expect_true(all(res["R_tot", , ] == 0))
   expect_true(any(res["D_tot", , ] > 0))
   expect_equal(sum(res["N_tot", , ] - sum(pars$N)), 0)
@@ -75,4 +75,36 @@ test_that("when beta_h = 0 there are only zoonotic infections", {
   expect_true(all(res[paste0("Eb", seq_len(pars$n_group - 1)), , ] == 0))
   expect_true(any(res["Ea18", , ] > 0))
   expect_true(any(res["Eb18", , ] > 0))
+})
+
+test_that("weekly outputs work as expected", {
+  pars <- reference_pars()
+  pars$beta_z[] <- 1e-4
+  pars$beta_h <- 0
+  m <- model$new(pars, 1, 3, seed = 1)
+  t <- seq(1, 22)
+  res <- m$simulate(t)
+  rownames(res) <- names(unlist(m$info()$index))
+
+  week_ends <- which(t %% 7 == 0)
+
+  # weekly cases reset every 7 days
+  expect_true(all(res["weekly_cases", , week_ends] >
+                    res["weekly_cases", , week_ends - 1]))
+  # cumulative cases match (N - S) = sum(weekly_cases)
+  expect_equal(res["N_tot", , max(t)] - res["S_tot", , max(t)],
+              rowSums(res["weekly_cases", , week_ends]))
+  # weekly cases by age sum to total
+  expect_equal(colSums(res[grep("weekly_cases", rownames(res)), , max(t)]),
+               res["weekly_cases", , max(t)] * 2)
+
+  # weekly deaths reset every 7 days
+  expect_true(all(res["weekly_deaths", , week_ends] >
+                    res["weekly_deaths", , week_ends - 1]))
+  # cumulative deaths match (N - S) = sum(weekly_deaths)
+  expect_equal(res["D_tot", , max(t)],
+               rowSums(res["weekly_deaths", , week_ends]))
+  # weekly deaths by age sum to total
+  expect_equal(colSums(res[grep("weekly_deaths", rownames(res)), , max(t)]),
+               res["weekly_deaths", , max(t)] * 2)
 })
