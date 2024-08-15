ca6ba4fa29b2c07e652cebb3837fee29916cdceb
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2024-08-15 15:00:52 +0100

    additional test to ensure vaccines given not exceeding vaccines allocated (related to last commit)

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -65,24 +65,24 @@ test_that("when beta_h = 0 there are only zoonotic infections", {
   pars <- reference_pars()
   pars$beta_h <- 0
   pars$beta_z<- c(rep(0,pars$n_group-1),0.4 / 12.11) # last group only for test purpose
-  
+
   m <- model$new(pars, 1, 3, seed = 1)
   t <- seq(1, 21)
   res <- m$simulate(t)
   rownames(res) <- names(unlist(m$info()$index))
-  
+
   ## isolate the compartments which can have infections (e.g. the last group)
   comps_of_interest_Ea <- paste0("Ea",
                                 seq(from=pars$n_group,
                                     length.out=pars$n_vax,by=pars$n_group))
   comps_of_interest_Eb <- paste0("Eb",seq(from=pars$n_group,
                                          length.out=pars$n_vax,by=pars$n_group))
-  
+
   expect_true(all(res[grep("^Ea",rownames(res)),,][!(rownames(res)[grep("^Ea",rownames(res))] %in% comps_of_interest_Ea)]==0))
   expect_true(all(res[grep("^Eb",rownames(res)),,][!(rownames(res)[grep("^Eb",rownames(res))] %in% comps_of_interest_Eb)]==0))
-  
+
   expect_equal(sum(res["N_tot", , ] - sum(pars$N)), 0)
-  
+
 })
 
 test_that("when n_vaccination>0, vaccinations are given", {
@@ -92,7 +92,7 @@ test_that("when n_vaccination>0, vaccinations are given", {
   t <- seq(1, 21)
   res <- m$simulate(t)
   rownames(res) <- names(unlist(m$info()$index))
-  
+
   comps_of_interest_S <- paste0("S",
                                 seq(from=pars$n_group+1,
                                     to=pars$n_group*pars$n_vax,by=1))
@@ -106,39 +106,56 @@ test_that("when n_vaccination>0, vaccinations are given", {
                                 seq(from=pars$n_group+1,
                                     to=pars$n_group*pars$n_vax,by=1))
 
-  
-  ## in first time point there should be no one vaccinated 
+
+  ## in first time point there should be no one vaccinated
   expect_true(all(res[comps_of_interest_S, ,1] == 0))
   expect_true(all(res[comps_of_interest_Ea, ,1] == 0))
   expect_true(all(res[comps_of_interest_Eb, ,1] == 0))
   expect_true(all(res[comps_of_interest_R, ,1] == 0))
-  
+
   ## for subsequent time points we should have people in these classes
   expect_true(any(res[comps_of_interest_S,,]>0))
   expect_true(any(res[comps_of_interest_Ea,,]>0))
   expect_true(any(res[comps_of_interest_Eb,,]>0))
   expect_true(any(res[comps_of_interest_R,,]>0))
-  
-  ## vaccines given should be positive 
+
+  ## vaccines given should be positive
   expect_true(any(res[grep("vax_given",rownames(res)),,]>0))
 
-  ## population check 
+  ## population check
   expect_equal(sum(res["N_tot", , ] - sum(pars$N)), 0)
-  
+
 })
 
 
 test_that("when time is after vaccination_campaign_length, no vaccines are given", {
   pars <- reference_pars()
-  
+
   m <- model$new(pars, 1, 3, seed = 1)
   t <- seq(1, 21)
   res <- m$simulate(t)
   rownames(res) <- names(unlist(m$info()$index))
-  
+
   expect_true(all(res[grep("vax_given",rownames(res)),,seq(pars$vaccination_campaign_length+1,max(t),1)]==0))
-  
-  
+
+
+})
+
+
+test_that("the vaccines given do not exceed the total set out in the strategy", {
+  pars <- reference_pars()
+
+  m <- model$new(pars, 1, 3, seed = 1)
+  t <- seq(1, 21)
+  res <- m$simulate(t)
+  rownames(res) <- names(unlist(m$info()$index))
+
+  # vaccines_planned <- sum(pars$n_vaccination)
+  # vaccines_given <- res["total_vax",,max(t)]
+
+  expect_true(all((sum(pars$n_vaccination) - res["total_vax",,max(t)])>0))
+
+
 })
 
 
