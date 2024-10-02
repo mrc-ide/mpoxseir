38403303bcc608d9f8d7174c1a0ba42f98ea4d62
edknock <edwardknock@gmail.com>
2024-10-02 14:14:41 +0100

    redocument

--- R/format_output.R
+++ R/format_output.R
@@ -20,14 +20,15 @@ rename_cols_rows <- function(m,res){
   return(res)
 }
 
+##' @importFrom dplyr %>%
 make_df_from_array <- function(m,res){
 
   # collapse from array into df format
   # add a column for variable as this can no longer be a rowname
   res_df <- plyr::adply(res,3,.id = "t") %>%
-    mutate(t = as.numeric(t),
+    dplyr::mutate(t = as.numeric(t),
            "var" = rep(names(unlist(m$info()$index)),max(t))) %>%
-    filter(var!="time")
+    dplyr::filter(var != "time")
 
   return(res_df)
 }
@@ -35,9 +36,9 @@ make_df_from_array <- function(m,res){
 df_long_form <- function(res_df){
 
   # collapse particle realisations
-  res_df_long <- pivot_longer(res_df,
-                            names_to = "particle",values_to="value",
-                            cols=starts_with("P"))
+  res_df_long <- tidyr::pivot_longer(res_df,
+                                     names_to = "particle",values_to="value",
+                                     cols = tidyr::starts_with("P"))
 
   return(res_df_long)
 
