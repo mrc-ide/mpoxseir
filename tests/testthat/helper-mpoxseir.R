4365dc2bc7590c32b9689f864af718c48a05ab3d
Lilith Whittles <lwhittles@users.noreply.github.com>
2024-10-11 16:31:55 +0100

    fix up tests and simplify helper function

--- tests/testthat/helper-mpoxseir.R
+++ tests/testthat/helper-mpoxseir.R
@@ -1,18 +1,19 @@
 
-reference_pars_targeted_vax <- function() {
-  pars <- parameters_fixed("equateur", initial_infections = 10)
+reference_pars_targeted_vax <- function(region = "equateur", uptake = 0) {
+  pars <- parameters_fixed(region, initial_infections = 10)
   n_group <- pars$n_group
   n_vax <- pars$n_vax
+  idx <- get_compartment_indices()
   
   pars$beta_h <- pars$beta_s <- 0.2 / 12.11
   pars$beta_z <- rep(0.4 / 12.11, n_group)
 
-  vaccination_campaign_length <- 10
-  daily_doses <- matrix(0, nrow = vaccination_campaign_length, ncol = n_vax)
+  pars$vaccination_campaign_length <- 10
+  daily_doses <- matrix(0, nrow = pars$vaccination_campaign_length, ncol = n_vax)
   # nothing happens for j=1 and 2 doses are the maximum
-  idx <- get_compartment_indices()
+
   daily_doses[, idx$vax$unvaccinated] <- daily_doses[, idx$vax$one_dose] <- 1000
-  daily_doses[vaccination_campaign_length, ] <- 0
+  daily_doses[pars$vaccination_campaign_length, ] <- 0
 
   pars$daily_doses <- daily_doses
   pars$N_prioritisation_steps <- 3
@@ -21,6 +22,7 @@ reference_pars_targeted_vax <- function() {
                                         c(rep(1,n_group))) # all
   pars$vaccination_coverage_target_1st_dose_prop <- 0.8
   pars$vaccination_coverage_target_2nd_dose_prop <- 0.5
+  pars$vaccine_uptake[] <- uptake
 
   pars
 }
