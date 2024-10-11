0ffe4e4a0c73e6ec92e36d699c767ca354d4aa11
Lilith Whittles <lwhittles@users.noreply.github.com>
2024-10-11 16:30:59 +0100

    set vaccine parameters to 0 in default params - we will vary this in onward simulation
    add helper functions to get compartment indices
    
    pull out use_ve_D as a parameter in parameters_fixed so that we can do fits allowing for different CFR by vaccine status, or not as required

--- tests/testthat/helper-mpoxseir.R
+++ tests/testthat/helper-mpoxseir.R
@@ -1,76 +1,28 @@
 
 reference_pars_targeted_vax <- function() {
-  dem_pars <- parameters_demographic()
-  n_group <- dem_pars$n_group
-  n_vax <- dem_pars$n_vax
-  Ea0 <- matrix(0, n_group, n_vax)
-  S0 <- Ea0
-  #S0[,2] <- dem_pars$N
-  S0[,2] <- rbinom(size=dem_pars$N,
-                   prob=dem_pars$sus_prop,n=length(dem_pars$N))
-  S0[,1] <- dem_pars$N-S0[,2]
-  # rowSums(S0)==dem_pars$N0
-  dem_pars$m_sex["SW","PBS"] <- dem_pars$m_sex["PBS","SW"] <- max(dem_pars$m_gen_pop)
+  pars <- parameters_fixed("equateur", initial_infections = 10)
+  n_group <- pars$n_group
+  n_vax <- pars$n_vax
+  
+  pars$beta_h <- pars$beta_s <- 0.2 / 12.11
+  pars$beta_z <- rep(0.4 / 12.11, n_group)
 
   vaccination_campaign_length <- 10
-  daily_doses <- matrix(0,ncol=n_vax,
-                        nrow=vaccination_campaign_length)
+  daily_doses <- matrix(0, nrow = vaccination_campaign_length, ncol = n_vax)
   # nothing happens for j=1 and 2 doses are the maximum
-  daily_doses[,2] <- daily_doses[,3] <- 1000
-  daily_doses[vaccination_campaign_length,] <- 0
+  idx <- get_compartment_indices()
+  daily_doses[, idx$vax$unvaccinated] <- daily_doses[, idx$vax$one_dose] <- 1000
+  daily_doses[vaccination_campaign_length, ] <- 0
 
-  N_prioritisation_steps <- 3
-  prioritisation_strategy <- cbind(c(1,1,1,rep(0,n_group-3)),# kids
-                                   c(1,1,1,rep(0,n_group-5),1,1), # kids + CSW
-                                   c(rep(1,n_group))) # all
-  vaccination_coverage_target_1st_dose_prop <- 0.8
-  vaccination_coverage_target_2nd_dose_prop <- 0.5
-  #n_group,N_prioritisation_steps,n_vax
-  # vaccination_coverage_target <- array(0,dim=c(n_group,N_prioritisation_steps,
-  #                                              n_vax))
-  #
-  # vaccination_coverage_target[,,3] <- round(prioritisation_strategy * vaccination_coverage_target_1st_dose_prop * dem_pars$N0)
-  # vaccination_coverage_target[,,4] <- round(prioritisation_strategy * vaccination_coverage_target_2nd_dose_prop * dem_pars$N0)
+  pars$daily_doses <- daily_doses
+  pars$N_prioritisation_steps <- 3
+  pars$prioritisation_strategy <- cbind(c(1,1,1,rep(0,n_group-3)),# kids
+                                        c(1,1,1,rep(0,n_group-5),1,1), # kids + CSW
+                                        c(rep(1,n_group))) # all
+  pars$vaccination_coverage_target_1st_dose_prop <- 0.8
+  pars$vaccination_coverage_target_2nd_dose_prop <- 0.5
 
-
-
-  out <- list(dt = 1,
-              N = dem_pars$N,
-              S0 = S0,
-              Ea0 = Ea0,
-              Eb0 = matrix(0, n_group, n_vax),
-              Ir0 = matrix(0, n_group, n_vax),
-              Id0 = matrix(0, n_group, n_vax),
-              R0 =  matrix(0, n_group, n_vax),
-              D0 =  matrix(0, n_group, n_vax),
-              beta_h = 0.2 / 12.11,
-              beta_s = 0.2 / 12.11,
-              beta_z = rep(0.4 / 12.11, n_group),
-              gamma_E = 0.05,
-              gamma_I = 0.1,
-              gamma_Ir = 0.1,
-              gamma_Id = 0.05,
-              CFR = matrix(c(1 / c(seq(2.5, 77.5, 5), 25, 25),
-                             (0.5*1) / c(seq(2.5, 77.5, 5), 25, 25)),
-                           n_group,n_vax),
-              ve_T = c(0.8,0,0.8,0.9),
-              ve_I = c(0.8,0,0.8,0.9),
-              m_gen_pop = dem_pars$m_gen_pop,
-              m_sex = dem_pars$m_sex,
-              n_group = n_group,
-              n_vax = n_vax,
-              #n_vaccination_allocation_SER = c(0.85,0.05,0.05,0.05),
-              vaccination_campaign_length = vaccination_campaign_length,
-              daily_doses = daily_doses,
-              N_prioritisation_steps = N_prioritisation_steps,
-              prioritisation_strategy = prioritisation_strategy,
-              #vaccination_coverage_target = vaccination_coverage_target,
-              vaccine_uptake = rep(1,n_group),
-              vaccination_coverage_target_1st_dose_prop = vaccination_coverage_target_1st_dose_prop,
-              vaccination_coverage_target_2nd_dose_prop = vaccination_coverage_target_2nd_dose_prop
-  )
-
-  return(out)
+  pars
 }
 
 
