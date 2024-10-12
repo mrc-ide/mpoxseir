b4b6ae6e732cd917c80a677482646092a2e02bbf
edknock <edwardknock@gmail.com>
2024-10-12 11:26:59 +0100

    cacth case where n_eligible_for_doseX is 0, avoid division by 0

--- inst/odin/model-targeted-vax.R
+++ inst/odin/model-targeted-vax.R
@@ -126,55 +126,62 @@ n_vaccination_t_Eb[, ] <- 0
 n_vaccination_t_R[, ] <- 0
 
 ## allocate 1st doses
-n_vaccination_t_S[, 2] <- min(
-  floor((daily_doses_t[1, 2] * S[i, 2] *
-           prioritisation_strategy[i, prioritisation_step_1st_dose] *
-           vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
-  S[i, 2])
+n_vaccination_t_S[, 2] <- if (sum(n_eligible_for_dose1[]) == 0) 0 else
+  min(floor((daily_doses_t[1, 2] * S[i, 2] *
+               prioritisation_strategy[i, prioritisation_step_1st_dose] *
+               vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
+      S[i, 2])
+  
 
-n_vaccination_t_Ea[, 2] <- min(
-  floor((daily_doses_t[1, 2] * Ea[i, 2] *
-           prioritisation_strategy[i, prioritisation_step_1st_dose] *
-           vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
-  Ea[i, 2])
+n_vaccination_t_Ea[, 2] <- if (sum(n_eligible_for_dose1[]) == 0) 0 else
+  min(floor((daily_doses_t[1, 2] * Ea[i, 2] *
+               prioritisation_strategy[i, prioritisation_step_1st_dose] *
+               vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
+      Ea[i, 2])
+  
 
-n_vaccination_t_Eb[, 2] <- min(
-  floor((daily_doses_t[1, 2] * Eb[i, 2] *
-           prioritisation_strategy[i, prioritisation_step_1st_dose] *
-           vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
-  Eb[i, 2])
+n_vaccination_t_Eb[, 2] <- if (sum(n_eligible_for_dose1[]) == 0) 0 else
+  min(floor((daily_doses_t[1, 2] * Eb[i, 2] *
+               prioritisation_strategy[i, prioritisation_step_1st_dose] *
+               vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
+      Eb[i, 2])
+  
 
-n_vaccination_t_R[, 2] <- min(
-  floor((daily_doses_t[1, 2] * R[i, 2] *
-           prioritisation_strategy[i, prioritisation_step_1st_dose] *
-           vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
-  R[i, 2])
+n_vaccination_t_R[, 2] <- if (sum(n_eligible_for_dose1[]) == 0) 0 else
+  min(floor((daily_doses_t[1, 2] * R[i, 2] *
+               prioritisation_strategy[i, prioritisation_step_1st_dose] *
+               vaccine_uptake[i]) / sum(n_eligible_for_dose1[])),
+      R[i, 2])
+  
 
 
 ## allocate 2nd doses
-n_vaccination_t_S[, 3] <- min(
+n_vaccination_t_S[, 3] <- if (sum(n_eligible_for_dose2[]) == 0) 0 else
+  min(
   floor((daily_doses_t[1, 3] * S[i, 3] *
            prioritisation_strategy[i, prioritisation_step_2nd_dose] *
            vaccine_uptake[i]) / sum(n_eligible_for_dose2[])),
   S[i, 3])
 
-n_vaccination_t_Ea[, 3] <- min(
-  floor((daily_doses_t[1, 3] * Ea[i, 3] *
-           prioritisation_strategy[i, prioritisation_step_2nd_dose] *
-           vaccine_uptake[i]) / sum(n_eligible_for_dose2[])),
-  Ea[i, 3])
+n_vaccination_t_Ea[, 3] <- if (sum(n_eligible_for_dose2[]) == 0) 0 else
+  min(floor((daily_doses_t[1, 3] * Ea[i, 3] *
+               prioritisation_strategy[i, prioritisation_step_2nd_dose] *
+               vaccine_uptake[i]) / sum(n_eligible_for_dose2[])),
+      Ea[i, 3])
+  
 
-n_vaccination_t_Eb[, 3] <- min(
-  floor((daily_doses_t[1, 3] * Eb[i, 3] *
-           prioritisation_strategy[i, prioritisation_step_2nd_dose] *
-           vaccine_uptake[i]) / sum(n_eligible_for_dose2[])),
-  Eb[i, 3])
+n_vaccination_t_Eb[, 3] <- if (sum(n_eligible_for_dose2[]) == 0) 0 else
+  min(floor((daily_doses_t[1, 3] * Eb[i, 3] *
+               prioritisation_strategy[i, prioritisation_step_2nd_dose] *
+               vaccine_uptake[i]) / sum(n_eligible_for_dose2[])),
+      Eb[i, 3])
+  
 
-n_vaccination_t_R[, 3] <- min(
-  floor((daily_doses_t[1, 3] * R[i, 3] *
-           prioritisation_strategy[i, prioritisation_step_2nd_dose] *
-           vaccine_uptake[i]) / sum(n_eligible_for_dose2[])),
-  R[i, 3])
+n_vaccination_t_R[, 3] <- if (sum(n_eligible_for_dose2[]) == 0) 0 else
+  min(floor((daily_doses_t[1, 3] * R[i, 3] *
+               prioritisation_strategy[i, prioritisation_step_2nd_dose] *
+               vaccine_uptake[i]) / sum(n_eligible_for_dose2[])),
+      R[i, 3])
 
 
 ## net vaccination change for relevant classes (S,Ea,Eb,R)
