ee00aa20b7aedbf885586663402ce35fdeabe3b3
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2025-02-04 10:57:59 +0000

    HCWs for bujumbura

--- R/parameters.R
+++ R/parameters.R
@@ -91,7 +91,7 @@ parameters_demographic <- function(region, mixing_matrix = "Zimbabwe") {
   ## HCW
   if(region %in% c("equateur","sudkivu")){
     p_HCW <- 133809 / sum(N_age)
-  } else if(region=="burundi"){
+  } else if(region %in% c("burundi","bujumbura")){
     p_HCW <- 107721 / sum(N_age)
   }
    
