10b4bef9797d823e0e6f8b731d4232d8a4f2e3a8
edknock <edwardknock@gmail.com>
2025-02-06 11:31:13 +0000

    add test

--- tests/testthat/test-model-targeted-vax.R
+++ tests/testthat/test-model-targeted-vax.R
@@ -909,7 +909,10 @@ test_that("Test compiled compare components", {
                   cases_SW = 10,
                   cfr_00_04 = 0.01,
                   cfr_05_14 = 0.03,
-                  cfr_15_plus = 0.05)
+                  cfr_15_plus = 0.05,
+                  cases_binom = 90,
+                  cases_00_04_binom = 12,
+                  cases_00_14_binom = 32)
   
   parts <- list(
     c("cases"),
@@ -924,7 +927,9 @@ test_that("Test compiled compare components", {
     c("cases_SW", "cases_total"),
     c("cfr_00_04"),
     c("cfr_05_14"),
-    c("cfr_15_plus"))
+    c("cfr_15_plus"),
+    c("cases_00_14_binom", "cases_binom"),
+    c("cases_00_04_binom", "cases_00_14_binom"))
 
   
   compare_part <- function(nms) {
