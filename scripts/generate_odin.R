84dd00fa870932296bf45ac03a304908507f6998
edknock <edwardknock@gmail.com>
2025-03-28 13:01:54 +0000

    generate with dust 0.3.22

--- scripts/generate_odin.R
+++ scripts/generate_odin.R
@@ -2,8 +2,8 @@
 if (packageVersion("odin2") < "0.3.19") {
   stop("Please upgrade odin2 to at least 0.3.19")
 }
-if (packageVersion("dust2") < "0.3.17") {
-  stop("Please upgrade dust2 to at least 0.3.17")
+if (packageVersion("dust2") < "0.3.22") {
+  stop("Please upgrade dust2 to at least 0.3.22")
 }
 
 odin2::odin_package(here::here())
