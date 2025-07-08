2a7985f8dcf6e2bf2076a9ffc416edfbd2f42688
edknock <edwardknock@gmail.com>
2025-07-08 12:12:07 +0100

    Recompile with latest odin2/dust2

--- scripts/generate_odin.R
+++ scripts/generate_odin.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
-if (packageVersion("odin2") < "0.3.24") {
-  stop("Please upgrade odin2 to at least 0.3.24")
+if (packageVersion("odin2") < "0.3.34") {
+  stop("Please upgrade odin2 to at least 0.3.34")
 }
-if (packageVersion("dust2") < "0.3.22") {
-  stop("Please upgrade dust2 to at least 0.3.22")
+if (packageVersion("dust2") < "0.3.24") {
+  stop("Please upgrade dust2 to at least 0.3.24")
 }
 
-odin2::odin_package(here::here())
+odin2::odin_package(here::here(), check_bounds = "disabled")
 devtools::load_all(here::here())
